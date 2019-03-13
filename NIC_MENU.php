20de69edfe379b5b2658a26ad0a744dcaf3e8524
Muyinda Rogers <moverr@gmail.com>
2019-03-13 18:30:03 +0300

    :Debugging :

--- NIC_MENU.php
+++ NIC_MENU.php
@@ -72,7 +72,7 @@ class NCBANKUSSD extends DynamicMenuController {
             $this->serviceDescription = $this->SERVICE_DESCRIPTION;
         } else {
 
-            $message = print_r($clientProfile['customerDetails'], true)
+            $message = print_r($clientProfile['customerDetails'], true);
 //            $message = "Welcome to NC Bank \n\n" . "Home Menu \n" . "1. Merchants \n" . "2. Balance Enquiry \n" . "3. Bill Payment \n" . "4. Funds Transfer \n" . "5. Bank to Mobile \n" . "6. Airtime Purchase \n" . "7. Mini statement \n" . "8. Cheque Requests \n" . "9. Change PIN \n";
 
             $this->displayText = $message;
