5b132450f411955a4102f77bdf51481be12837ab
Muyinda Rogers <moverr@gmail.com>
2019-03-13 19:49:46 +0300

    :Debugigng :

--- NIC_MENU.php
+++ NIC_MENU.php
@@ -76,7 +76,7 @@ class NCBANKUSSD extends DynamicMenuController {
 
 //            accountDetails
 //            $message = print_r($clientProfiledata, true);
-//            $message = "Welcome to NC Bank \n\n" . "Home Menu \n" . "1. Merchants \n" . "2. Balance Enquiry \n" . "3. Bill Payment \n" . "4. Funds Transfer \n" . "5. Bank to Mobile \n" . "6. Airtime Purchase \n" . "7. Mini statement \n" . "8. Cheque Requests \n" . "9. Change PIN \n";
+            $message = "Welcome to NC Bank \n\n" . "Home Menu \n" . "1. Merchants \n" . "2. Balance Enquiry \n" . "3. Bill Payment \n" . "4. Funds Transfer \n" . "5. Bank to Mobile \n" . "6. Airtime Purchase \n" . "7. Mini statement \n" . "8. Cheque Requests \n" . "9. Change PIN \n";
 
             $this->displayText = $message;
             $this->sessionState = "CONTINUE";
