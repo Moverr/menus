16724c7b43bb0c84c73c5dc075cf7f16cfe97186
Muyinda Rogers <moverr@gmail.com>
2019-03-18 21:08:11 +0300

    :debuigigng :

--- NIC_MENU.php
+++ NIC_MENU.php
@@ -444,11 +444,23 @@ class NCBANKUSSD extends DynamicMenuController {
                 $this->displayText = $message;
                 $this->sessionState = "CONTINUE";
                 $this->serviceDescription = $this->SERVICE_DESCRIPTION;
-                $this->nextFunction = "TopUpAmountMenu";
+                $this->nextFunction = "AirtimeMerchantChooseAccount";
                 $this->previousPage = "AirtimePurchaseMenu";
 
                 break;
-            case '2':break;
+            case '2':
+                 $message = "Enter Top Up Amount";
+                $message .= "\n\n0. Home \n" . "00. Back \n" . "000. Logout \n";
+
+                $this->displayText = $message;
+                $this->sessionState = "CONTINUE";
+                $this->serviceDescription = $this->SERVICE_DESCRIPTION;
+                $this->nextFunction = "TopUpAmountMenu";
+                $this->previousPage = "AirtimePurchaseMenu";
+
+                
+                
+                break;
             case '0':
                 $this->firstMenu();
                 break;
