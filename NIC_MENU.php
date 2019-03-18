3778ee97ff1c34c8369abcfed88da1649e95bbf4
Muyinda Rogers <moverr@gmail.com>
2019-03-18 12:40:24 +0300

    :Debugging :

--- NIC_MENU.php
+++ NIC_MENU.php
@@ -21,7 +21,7 @@ class NCBANKUSSD extends DynamicMenuController {
     );
     private $SERVICE_DESCRIPTION = "NC BANK MENU ";
     private $walletUrl = 'http://132.147.160.57:8300/wallet/IS_APIs/CustomerRegistration/fetchCustomerData';
-     private $serverURL = 'http://132.147.160.57:8300/wallet/Cloud_APIs/index';
+     private $serverURL ='http://132.147.160.57:8300/wallet/IS_APIs/index';
                 
     
 
