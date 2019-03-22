470699bf2b085d9df88d1d6eee413b8ba4130c87
Muyinda Rogers <moverr@gmail.com>
2019-03-22 09:32:56 +0300

    :Debugging :

--- NIC_MENU.php
+++ NIC_MENU.php
@@ -790,7 +790,7 @@ class NCBANKUSSD extends DynamicMenuController {
 
                 $logRequest = $this->logChannelRequest($requestPayload, $this->STATUS_CODE, NULL, 359);
                 $result = $this->synchronousProcessing($requestPayload, $logRequest['LAST_INSERT_ID']);
-                $message = " Account Not Found" . (print_r(json_decode($result), true));
+                $message = " Account Not Found" . (print_r(xmlrpc_decode($result), true));
 
 
 
