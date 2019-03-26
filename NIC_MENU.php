f5b22b684adab925299684af2adf461500701200
Muyinda Rogers <moverr@gmail.com>
2019-03-26 20:41:38 +0300

    :Debuggging :

--- NIC_MENU.php
+++ NIC_MENU.php
@@ -944,7 +944,7 @@ class NCBANKUSSD extends DynamicMenuController {
             );
             $logRequest = $this->logChannelRequest($requestPayload, $this->STATUS_CODE, NULL, 359);
             $result = $this->invokeSyncWallet($requestPayload, $logRequest['DATA']['LAST_INSERT_ID']);
-            $this->displayText = "" . print_r($result['STAT_DESCRIPTION'], true);
+            $this->displayText = "" . $result['STAT_DESCRIPTION'];
             $this->sessionState = "END";
         }
     }
