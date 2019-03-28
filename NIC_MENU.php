cb4a05bb053cda502dc7118c5bdc11e315b6faaa
Muyinda Rogers <moverr@gmail.com>
2019-03-28 09:25:54 +0300

    :Debuguing :

--- NIC_MENU.php
+++ NIC_MENU.php
@@ -2585,12 +2585,12 @@ class NCBANKUSSD extends DynamicMenuController {
 
         $responseArray = json_decode($response, true);
 
-        $responseData = $responseArray['results'][0]['responseExtraData'];
-//
-        $responseDataArray = json_decode($responseData, true);
-//
+//        $responseData = $responseArray['results'][0]['responseExtraData'];
+////
+//        $responseDataArray = json_decode($responseData, true);
+////
 
-        return $responseDataArray;
+        return $responseArray;
     }
 
     function postValidationRequestToHUB($url, $fields) {
