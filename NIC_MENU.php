94fdbce2a7e91b8298378adad1dc4cf860bbdb69
Muyinda Rogers <moverr@gmail.com>
2019-04-01 15:17:50 +0300

    :Debuggigng :

--- NIC_MENU.php
+++ NIC_MENU.php
@@ -608,7 +608,8 @@ class NCBANKUSSD extends DynamicMenuController {
                     $response = json_decode($result);
 //                $this->displayText = "" . print_r($result, true); 
 //                    $this->logMessage("Validate Customer PIN Response:: ", $response, 4);
-
+                
+                    
                     $message = "RESPONSE : " . print_r($response->STAT_DESCRIPTION,true);
 
 //                    $message .= "\n\n0. Home \n" . "00. Back";
