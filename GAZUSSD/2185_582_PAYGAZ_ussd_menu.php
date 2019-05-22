ff66b4fb31b6b1b3b78526479343aed77738cc4e
Muyinda Rogers <moverr@gmail.com>
2019-05-22 22:30:50 +0300

    :Debugging :

--- GAZUSSD/2185_582_PAYGAZ_ussd_menu.php
+++ GAZUSSD/2185_582_PAYGAZ_ussd_menu.php
@@ -221,20 +221,17 @@
         );
 
 
-    $respponse = $this->postValidationRequestToHUB("http://10.250.250.29:9000/hub/services/paymentGateway/JSON/index.php",son_encode($spayload));
+            // $respponse = $this->postValidationRequestToHUB($this->$hubJSONAPIUrl, json_encode($spayload));
 
 
 
 // $payload = array(
 //     'credentials' => $credentials,
 //     'packet' => $packet);
-// //$params=array($function,$payload);
-// //private $hubJSONAPIUrl = "http://197.159.100.247:9000/hub/services/paymentGateway/JSON/index.php";
-// //http://197.159.100.247:9000/hub/services/paymentGateway/JSON/index.php
-//  $response  =  $this->postToCPGPayload($payload, "http://10.250.250.29:9000/hub/services/paymentGateway/JSON/index.php", "BEEP.postPayment");
-
-
-
+//$params=array($function,$payload);
+//private $hubJSONAPIUrl = "http://197.159.100.247:9000/hub/services/paymentGateway/JSON/index.php";
+//http://197.159.100.247:9000/hub/services/paymentGateway/JSON/index.php
+ $response  =  $this->postToCPGPayload($spayload, "http://10.250.250.29:9000/hub/services/paymentGateway/JSON/index.php", "BEEP.postPayment");
 // return array("SUCCESS"=>true);
 
 
@@ -296,7 +293,10 @@ function postToCPGPayload($params, $url, $method) {
 
          function postValidationRequestToHUB($url, $fields) {
              $fields_string = null;
- 
+ $this->logMessage(" .......................: ", "INIT");
+
+
+
         $ch = curl_init(); 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -305,7 +305,9 @@ function postToCPGPayload($params, $url, $method) {
       
         $result = curl_exec($ch); 
         curl_close($ch);
-       
+         $this->logMessage(" .......................: ", json_decode($result));
+
+
 
           return $result;
         }
