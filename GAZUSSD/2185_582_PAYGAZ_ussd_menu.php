4654c24f23317ff4fb46704d48a07a26fdd838fd
Muyinda Rogers <moverr@gmail.com>
2019-05-22 16:22:21 +0300

    :Debugging :

--- GAZUSSD/2185_582_PAYGAZ_ussd_menu.php
+++ GAZUSSD/2185_582_PAYGAZ_ussd_menu.php
@@ -173,7 +173,7 @@
             "authorization"=>"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdhelRvcFVwLm11bGFAY2VsbHVsYW50LmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJDJCM2IzY1luWG5yOVRkSlhWbUgwQU8ydXhSSkUvRFY3L0NuSTYzS3RycVNVZWNIQ1R1cEsyIiwidXNlcm5hbWUiOiJNdWxhX0NlbGx1bGFudCIsInJhbmRvbSI6Ik1VTEFfR0FaNDI4NjYxMTIzIiwiaWF0IjoxNTUzNzg0NTAxfQ.WBpkwbMWuRx5sgqjkmAuwgvaG1dFrduoY2bhdmi2EDw",
             "cardmask"=>"G001",
             "transactioncode"=>"CPG_TRANSACTION",
-            "amount"=>234000
+            "amount"=>12345
 
  
 
@@ -265,7 +265,7 @@ $result = ($respponse);
       
         $result = curl_exec($ch); 
         curl_close($ch);
-          return "pass";
+          return $result;
         }
 
 
