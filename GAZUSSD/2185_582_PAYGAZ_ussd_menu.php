1f9c9489b4856931daf3ba264e9b0bedb136536e
Muyinda Rogers <moverr@gmail.com>
2019-05-22 11:03:15 +0300

    :Menu Setup default :

--- GAZUSSD/2185_582_PAYGAZ_ussd_menu.php
+++ GAZUSSD/2185_582_PAYGAZ_ussd_menu.php
@@ -1,120 +1,134 @@
-        <?php
+            <?php
 
-        /*
-         * GAZ USSD Collections
-         *
-         * @author Muyinda ROgers
-         *
-         */
+            /*
+             * GAZ USSD Collections
+             *
+             * @author Muyinda ROgers
+             *
+             */
 
-        include 'DynamicMenuController.php';
-        include './NCUGconfigs.php';
+            include 'DynamicMenuController.php';
+            include './NCUGconfigs.php';
 
-        class GAZUSSD extends DynamicMenuController {
+            class GAZUSSD extends DynamicMenuController {
 
-            private $MENU_STATUS = true;
-            private $KEY_CUSTOMER_MOBILE_NUMBER = "customerMobileNumber";
-            private $KEY_AMOUNT = "amount";
-            private $MAX_AMOUNT = 4000000;
-            private $MIN_AMOUNT = 500;
-            private $STATUS_CODE = 2;
-            private $MERCHANT_WHITELIST = array();
-            private $SERVICE_DESCRIPTION = "NC BANK MENU ";
-            private $walletUrl = 'http://132.147.160.57:8300/wallet/IS_APIs/CustomerRegistration/fetchCustomerData';
-            private $serverURL = 'http://132.147.160.57:8300/wallet/Cloud_APIs/index';
-        //    private $accessPoint = "*268#";
-            private $accessPoint = "NIC_UG";
-        //            "*268#";
-            private $IMCREQUESTID = 1;
-            private $SAMPLEMSSDN = '256783262929';
-            private $USERNAME = "system-user";
-            private $PASSWORD = "lipuka";
-            //regex configs
-            private $phone_reg = "/^(25677|25678|25639|25671|25670|25675|25679|77|78|39|71|70|75|79|077|078|039|071|070|075|079)[0-9]{7}$/";
-            private $mtn_reg = "/^(77|78|39|25677|25678|25639|077|078|039)(\d{7})$/";
-            private $airtel_reg = "/^(075|25675|75|25670|70|070)(\d{7})$/";
-            private $warid_reg = "/^(25670|70|070)(\d{7})$/";
-            private $utl_reg = "/^(71|071|25671)(\d{7})$/";
-            private $orange_reg = "/^(079|25679|79)(\d{7})$/";
-            //validation configs
-            // private $hubJSONAPIUrl = "http://localhost:9001/hub/services/paymentGateway/JSON/index.php";
-            private $hubJSONAPIUrl = "http://197.159.100.247:9000/hub/services/paymentGateway/JSON/index.php";
-            private $hubValidationFunction = "BEEP.validateAccount";
-            private $hubAuthSuccessCode = "131";
-            private $hubValidationSuccessCode = "307";
-            private $beepUsername = "nic_test_api_user";
-            private $beepPassword = "nic_t3st_api_us3r";
-            private $umemeServiceID = '28';
-            private $umemeServiceCode = 'UMEME';
-            private $nwscServiceID = '27';
-            private $nwscServiceCode = 'NWSC';
-            private $kccaServiceID = '233';
-            private $kccaServiceCode = 'KCCA';
-            private $uraServiceID = '30';
-            private $uraServiceCode = 'URA';
-            private $nwscAreas = "Kampala,Jinja,Entebbe,Lugazi,Iganga,Kawuku,Kajjansi,Mukono,Others";
+                private $MENU_STATUS = true;
+                private $KEY_CUSTOMER_MOBILE_NUMBER = "customerMobileNumber";
+                private $KEY_AMOUNT = "amount";
+                private $MAX_AMOUNT = 4000000;
+                private $MIN_AMOUNT = 500;
+                private $STATUS_CODE = 2;
+                private $MERCHANT_WHITELIST = array();
+                private $SERVICE_DESCRIPTION = "NC BANK MENU ";
+                private $walletUrl = 'http://132.147.160.57:8300/wallet/IS_APIs/CustomerRegistration/fetchCustomerData';
+                private $serverURL = 'http://132.147.160.57:8300/wallet/Cloud_APIs/index';
+            //    private $accessPoint = "*268#";
+                private $accessPoint = "NIC_UG";
+            //            "*268#";
+                private $IMCREQUESTID = 1;
+                private $SAMPLEMSSDN = '256783262929';
+                private $USERNAME = "system-user";
+                private $PASSWORD = "lipuka";
+                //regex configs
+                private $phone_reg = "/^(25677|25678|25639|25671|25670|25675|25679|77|78|39|71|70|75|79|077|078|039|071|070|075|079)[0-9]{7}$/";
+                private $mtn_reg = "/^(77|78|39|25677|25678|25639|077|078|039)(\d{7})$/";
+                private $airtel_reg = "/^(075|25675|75|25670|70|070)(\d{7})$/";
+                private $warid_reg = "/^(25670|70|070)(\d{7})$/";
+                private $utl_reg = "/^(71|071|25671)(\d{7})$/";
+                private $orange_reg = "/^(079|25679|79)(\d{7})$/";
+                //validation configs
+                // private $hubJSONAPIUrl = "http://localhost:9001/hub/services/paymentGateway/JSON/index.php";
+                private $hubJSONAPIUrl = "http://197.159.100.247:9000/hub/services/paymentGateway/JSON/index.php";
+                private $hubValidationFunction = "BEEP.validateAccount";
+                private $hubAuthSuccessCode = "131";
+                private $hubValidationSuccessCode = "307";
+                private $beepUsername = "nic_test_api_user";
+                private $beepPassword = "nic_t3st_api_us3r";
+                private $umemeServiceID = '28';
+                private $umemeServiceCode = 'UMEME';
+                private $nwscServiceID = '27';
+                private $nwscServiceCode = 'NWSC';
+                private $kccaServiceID = '233';
+                private $kccaServiceCode = 'KCCA';
+                private $uraServiceID = '30';
+                private $uraServiceCode = 'URA';
+                private $nwscAreas = "Kampala,Jinja,Entebbe,Lugazi,Iganga,Kawuku,Kajjansi,Mukono,Others";
 
-            function startPage() {
-
-
+                function startPage() {
 
 
 
-                $message = "Select Option. \n1: VERIFY CARD \n2: TOPUP CARD \n3: CARD BALANCE \n4: CARD MINISTATEMET \n\n 0) EXIT";
-
-                $this->displayText = $message;
-                $this->sessionState = "CONTINUE";
-                $this->nextFunction = "menuSwiter";
-                $this->previousPage = "startPage";
 
 
+                    $message = "Select Option. \n1: VERIFY CARD \n2: TOPUP CARD \n3: CARD BALANCE \n4: CARD MINISTATEMET \n\n 0) EXIT";
 
-            }
+                    $this->displayText = $message;
+                    $this->sessionState = "CONTINUE";
+                    $this->nextFunction = "menuSwiter";
+                    $this->previousPage = "startPage";
 
-            function  menuSwiter($input){
-                switch ($input) {
-                    case '1':
-                        # code...
-                    break;
-                    case '2':
-                        # code...
-                    break;
-                    case '3':
-                        # code...
-                    break;
-                    case '4':
-                        # code...
-                    break;
 
-                    
-                    default:
-                        # code...
-                    break;
+
                 }
+
+                function  menuSwiter($input){
+                    switch ($input) {
+                        case '1':
+                            # code...
+                               $this->displayText = "Enter Card Number";
+                                $this->sessionState = "CONTRINUE";
+
+                        break;
+                        case '2':
+                             $this->displayText = "Enter Card Number";
+                                $this->sessionState = "CONTRINUE";
+
+                        break;
+                        case '3':
+                            $this->cardBalanceMenu();
+                        break;
+                        case '4':
+                           $this->cardMiniStatementMenu();
+                        break;
+
+                        
+                        default:
+                            $this->ehdMenu();
+                        break;
+                    }
+                }
+
+
             }
 
+            function validateCardMenu(){
+                //todo: Enter Card Number
+               
+            }
+            function topUpCardMenu(){
 
-        }
+            }
 
-        function validateCardMenu(){
+            function validateCard($input){
 
-        }
-        function topUpCardMenu(){
+            }
 
-        }
-        function cardBalanceMenu(){
+            function cardBalanceMenu(){
+               $this->displayText = "COMING SOON";
+               $this->sessionState = "END";
+           }
+           function cardMiniStatementMenu(){
+               $this->displayText = "COMING SOON";
+               $this->sessionState = "END";
+           }
 
-        }
-        function cardMiniStatementMenu(){
-
-        }
-
-        function ehdMenu(){
-            
-        }
+           function ehdMenu(){
+               $this->displayText = "Thank you for using Gaz";
+               $this->sessionState = "END";
+           }
 
 
 
-        $ncBankUSSD = new GAZUSSD;
-        echo $ncBankUSSD->navigate();
+           $ncBankUSSD = new GAZUSSD;
+           echo $ncBankUSSD->navigate();
 
