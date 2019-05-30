a68e7ef50cee1d10a7bbc764505815d454286b77
Muyinda Rogers <moverr@gmail.com>
2019-05-30 12:33:51 +0300

    :ebuggig :

--- NCUGconfigs.php
+++ NCUGconfigs.php
@@ -13,7 +13,6 @@
  */
 class NCUGconfigs {
 
-//    const PIN_SERVICE_ID = 206;
 	const PIN_SERVICE_ID = 6;
 	const QUESTIONNUMBER = 3;
 	const DEFAULT_PIN = '1234';
@@ -23,7 +22,7 @@ class NCUGconfigs {
 	const LOG_LEVEL_INFO = 4;
 	const LOG_LEVEL_ERROR = 1;
 	const PIN_EXPIRE_TIME = 86400;
-	const BANK_SIGNATURE = "NC BANK UGANDA";
+	const BANK_SIGNATURE = "DTB Uganda";
 	const PANAMA_REQ_PARAM_NUM = "4";
 	const HUB_CLIENT_ID = 186;
 	const UG_CODE = 256;
@@ -35,40 +34,45 @@ class NCUGconfigs {
 
 	//PayTV configs
 	const DEFAULT_PACKAGE_NAME = " ";
-//   const GOTV_PACKAGES = "GOtv Lite=9500, Mobile Bouquet=11250, Lite Quarterly=22000, GOtv Plus=26000, Max Bouquet=49000, Lite 12mths=60000";
-	const GOTV_PACKAGES = "GOtv Lite 1=11000, GOtv Value=16000, GOtv Lite 3=25000, GOtv Plus=28000, GOtv Max=49000, GOtv Lite 12=70000";
+	const GOTV_PACKAGES = "GOtv Lite=9500, Mobile Bouquet=11250, Lite Quarterly=22000, GOtv Plus=26000, Max Bouquet=49000, Lite 12mths=60000";
 	const DSTV_PACKAGES = "Access=33000, Family=60000, French11=66375, Asia=128000, Compact=115000, Compact Plus=180000, Premium=280000";
 	const AZAM_PACKAGES = "Azam 1wk Pure=3500, Azam 2wks Pure=6500, Azam Pure=10000, Azam Plus=25000, Azam Play=37000";
 	const STARTIMES_PACKAGES = "Basic=18000, Classic=33000, Unique=60000";
-//const ZUKU_PACKAGES = "Smart=10500, Classic=23500, Premium=33500";
-	const ZUKU_PACKAGES = "Smart=12000, Classic=26500, Premium=36500";
+	const ZUKU_PACKAGES = "Smart=10500, Classic=23500, Premium=33500";
 	const KWESE_PACKAGES = "3 days=17000, 7 days=32000, 30 days=106000";
 
-	const DSTV_SERVICE_ID = 83;
-	const DSTV_SERVICE_CODE = "DSTV";
+	const DSTV_SERVICE_ID = 33;
+	const DSTV_SERVICE_CODE = "DSTVUG";
 	const DSTVUG_CODE = "DSTV";
 	const DSTV_WALLET_MERCHANT_CODE = "DSTV_UGANDA";
-	const STARTIMES_SERVICE_ID = 85;
-	const STARTIMES_SERVICE_CODE = "STARTIMES";
+
+	const STARTIMES_SERVICE_ID = 1298;
+	const STARTIMES_SERVICE_CODE = "STARTIMESUG";
 	const STARTIMES_CODE = "STARTIMES";
 	const STARTIMES_WALLET_MERCHANT_CODE = "STARTIMESUG";
-	const ZUKU_SERVICE_ID = 86;
-	const ZUKU_SERVICE_CODE = "ZUKU";
+
+	const ZUKU_SERVICE_ID = 35;
+	const ZUKU_SERVICE_CODE = "ZUKUUG";
 	const ZUKU_CODE = "ZUKU";
 	const ZUKU_WALLET_MERCHANT_CODE = "ZUKUTV";
-	const GOTV_SERVICE_ID = 84;
+
+	const GOTV_SERVICE_ID = 34;
 	const GOTV_SERVICE_CODE = "GOTV";
 	const GOTV_CODE = "GOTV";
 	const GOTV_WALLET_MERCHANT_CODE = "GotvUG";
-	const AZAM_SERVICE_ID = 76;
+
+	const AZAM_SERVICE_ID = 790;
 	const AZAM_SERVICE_CODE = "AZAMTV";
 	const AZAM_CODE = "AZAMTV";
 	const AZAM_WALLET_MERCHANT_CODE = "AZAMTV";
-	const KWESE_SERVICE_ID = 77;
+
+	const KWESE_SERVICE_ID = 834;
 	const KWESE_SERVICE_CODE = "KWESETV";
 	const KWESE_CODE = "KWESE";
 	const KWESE_WALLET_MERCHANT_CODE = "KWESEUG";
+
 	const BILLPAY_SERVICE = "BILL_PAY";
+//    $data =  array("233"=>"KCCA","28"=>"UMEME","27"=>"NWSC","30"=>"URA", "33"=>"DSTVUG", "34"=>"GOTV", "557"=>"NSSF","790"=>"AZAMTV","834"=>"KWESETV","641"=>"zukku","1298"=>"STARTIMESUG");
 
 	const MAX_NARRATION_LENGTH = 50;
 
