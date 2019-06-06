f6bda3a86431bd0431b165d1326c36290ea849be
Muyinda Rogers <moverr@gmail.com>
2019-06-06 13:14:28 +0300

    :Debugging l:

---
+++ Processors/paymentPusher/tests/GazProcessorTest.php
@@ -0,0 +1,17 @@
+ <?php
+class GazProcessorTest extends PHPUnit_Framework_TestCase {
+	public function setUp() {
+		// your code here
+	}
+
+	public function tearDown() {
+		// your code here
+	}
+
+	public function testComparesNumbers() {
+		$this->assertTrue(1 == 1);
+	}
+
+}
+
+?>
